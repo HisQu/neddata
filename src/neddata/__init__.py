0a40c9adea9465280447e582ef4a64d751f77176
markur4 <martin.kur4@gmail.com>
2025-06-30 10:27:12 +0200

    ✨ feat(neddata): flatten access to core components
    - import make_pooch, make_pooch_registry, Resource, DataFile, DataDir, and Catalog from neddata.datamodel for easier access
    - retain existing import of abbey_catalog for backward compatibility

--- src/neddata/__init__.py
+++ src/neddata/__init__.py
@@ -1,1 +1,14 @@
+#
+### Flatten Access
+
+from neddata.datamodel import (
+    make_pooch,
+    make_pooch_registry,
+    Resource,
+    DataFile,
+    DataDir,
+    Catalog,
+)
+
+
 from neddata.abbey.catalog import cat as abbey_catalog
